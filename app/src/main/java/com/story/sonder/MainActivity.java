9975e1cb52773c5d64a49a320bc23999c6105919
MipsaPatel <mipsa.p.patel@gmail.com>
2018-03-29 17:23:49 +0530

    Options menu added.

--- app/src/main/java/com/story/sonder/MainActivity.java
+++ app/src/main/java/com/story/sonder/MainActivity.java
@@ -2,6 +2,9 @@ package com.story.sonder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.tag:
+                break;
+
+            case R.id.filter:
+                break;
+
+            case R.id.about_us:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
