860fe3b7eb84e9f7db89ba05ad828e5e9dc8da7a
TilakSN <tilakchinnu96@gmail.com>
2018-04-15 03:59:13 +0530

    Adam and SGD implemented.

--- app/src/main/java/com/story/sonder/model/optimizer/Adam.java
+++ app/src/main/java/com/story/sonder/model/optimizer/Adam.java
@@ -3,20 +3,61 @@ package com.story.sonder.model.optimizer;
 import com.story.sonder.model.Tensor;
 import com.story.sonder.model.layer.ILayer;
 
+import java.util.List;
+
 public class Adam extends Optimizer implements IOptimizer {
-    public Adam(ILayer model) {
+    private final double learningRate;
+    private final double rho1;
+    private final double rho2;
+    private final double delta;
+    private double pr1;
+    private double pr2;
+
+    public Adam(ILayer model, double learningRate, double rho1, double rho2, double delta) {
         super(model);
-        // TODO: set hyper parameters
+        this.learningRate = learningRate;
+        this.rho1 = rho1;
+        this.rho2 = rho2;
+        this.delta = delta;
+        pr1 = rho1;
+        pr2 = rho2;
+        for (List<Tensor> parameter : parameters) {
+            Tensor zeroes = new Tensor(parameter.get(0).getShape());
+            parameter.add(zeroes);
+            parameter.add(zeroes.copy());
+        }
+    }
+
+    public Adam(ILayer model, double learningRate) {
+        this(model, learningRate, 0.9, 0.999, 1e-8);
+    }
+
+    public Adam(ILayer model) {
+        this(model, 0.001);
     }
 
     @Override
     public void learn(Tensor... parameters) {
-        // TODO: update parameters
+        Tensor w = parameters[0], g = parameters[1], s = parameters[2], r = parameters[3];
+        w.updateEach((i, v) -> {
+                    double grad = g.getValueAt(i);
+                    s.setValueAt(i, rho1 * s.getValueAt(i) * (1 - rho1) * grad);
+                    r.setValueAt(i, rho2 * r.getValueAt(i) + (1 - rho2) * grad * grad);
+                    return v - learningRate * s.getValueAt(i) /
+                            ((1 - pr1) * (Math.sqrt(r.getValueAt(i) / (1 - pr2)) + delta));
+                }
+        );
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        pr1 *= rho1;
+        pr2 *= rho2;
     }
 
     @Override
     public String toString() {
-        // TODO: fill in the parameters
-        return "Adam(learning_rate=, rho1=, rho2=, delta=)";
+        return "Adam(learningRate=" + learningRate + ", rho1=" + rho1 + ", rho2=" + rho2 + ", delta=" + delta + ")";
     }
 }
--- app/src/main/java/com/story/sonder/model/optimizer/SGD.java
+++ app/src/main/java/com/story/sonder/model/optimizer/SGD.java
@@ -4,19 +4,21 @@ import com.story.sonder.model.Tensor;
 import com.story.sonder.model.layer.ILayer;
 
 public class SGD extends Optimizer implements IOptimizer {
-    public SGD(ILayer model) {
+    private final double learningRate;
+
+    public SGD(ILayer model, double learningRate) {
         super(model);
-        // TODO: set hyper parameters
+        this.learningRate = learningRate;
     }
 
     @Override
     public void learn(Tensor... parameters) {
-        // TODO: update parameters
+        Tensor g = parameters[1];
+        parameters[0].updateEach((i, v) -> v - learningRate * g.getValueAt(i));
     }
 
     @Override
     public String toString() {
-        // TODO: fill in the parameters
-        return "SGD(learning_rate=)";
+        return "SGD(learning_rate=" + learningRate + ")";
     }
 }
