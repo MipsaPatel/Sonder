b3e913f0cc9648ae1bff7cf859af77a043c62756
TilakSN <tilakchinnu96@gmail.com>
2018-04-23 21:50:49 +0530

    Quit application if storage permission is denied.

--- app/src/main/java/com/story/sonder/MainActivity.java
+++ app/src/main/java/com/story/sonder/MainActivity.java
@@ -118,6 +118,7 @@ public class MainActivity extends Activity {
                             "STORAGE PERMISSION DENIED. App will not function.",
                             Toast.LENGTH_SHORT
                     ).show();
+                    finishAndRemoveTask();
                 }
                 break;
 
