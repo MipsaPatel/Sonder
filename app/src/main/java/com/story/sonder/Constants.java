77f73db5558e573ab84d4f421ae60f0a70070e98
TilakSN <tilakchinnu96@gmail.com>
2018-04-26 03:16:16 +0530

    Updated mean and standard deviation of pixel values.

--- app/src/main/java/com/story/sonder/Constants.java
+++ app/src/main/java/com/story/sonder/Constants.java
@@ -20,10 +20,8 @@ class Constants {
     static final String paramsFile = "params.json";
     static final String serverUrl = "http://four-d.tech/model";
 
-    static final int numClasses = 10;
-
-    static final double[] mean = new double[]{0.498420, 0.458495, 0.397866};
-    static final double[] std = new double[]{0.313047, 0.269076, 0.231632};
+    static final double[] mean = new double[]{0.500424, 0.456539, 0.410315};
+    static final double[] std = new double[]{0.250922, 0.240401, 0.269415};
 
     static final int inputWidth = 32;
     static final int inputHeight = 32;
